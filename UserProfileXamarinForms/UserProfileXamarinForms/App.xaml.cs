f7103fe3595398f44fbecb4b99a191444efaf024
Adam Anderson <andersad@gmail.com>
2019-10-03 16:14:48 -0400

    Add navigation page and update themes

--- UserProfileXamarinForms/UserProfileXamarinForms/App.xaml.cs
+++ UserProfileXamarinForms/UserProfileXamarinForms/App.xaml.cs
@@ -10,7 +10,7 @@ namespace UserProfileXamarinForms
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
