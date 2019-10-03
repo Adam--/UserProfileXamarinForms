7b31e76f64c4f4863beedbce6abcee0c30843bb4
Adam Anderson <andersad@gmail.com>
2019-10-03 15:36:48 -0400

    Add light and dark themes

---
+++ UserProfileXamarinForms/UserProfileXamarinForms/Theme/DarkTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace UserProfileXamarinForms.Theme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DarkTheme
+    {
+        public DarkTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UserProfileXamarinForms/UserProfileXamarinForms/Theme/LightTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace UserProfileXamarinForms.Theme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LightTheme
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
